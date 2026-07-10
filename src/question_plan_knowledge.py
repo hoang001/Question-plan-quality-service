cf7b89984a8802c9e8ff81359003dca266ff92bc
hoang001st <155293385+hoang001@users.noreply.github.com>
2026-07-10 08:26:46 +0700

    Remove legacy root files

--- src/question_plan_knowledge.py
+++
@@ -1,7 +0,0 @@
-"""Backward-compatible alias for question plan evaluation knowledge."""
-
-from .question_plan_eval_knowledge import QUESTION_PLAN_EVAL_KNOWLEDGE
-
-
-QUESTION_PLAN_KNOWLEDGE = QUESTION_PLAN_EVAL_KNOWLEDGE
-
